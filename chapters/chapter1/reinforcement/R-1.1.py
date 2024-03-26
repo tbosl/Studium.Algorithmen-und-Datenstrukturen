5f5a6f678615c7dc7e53a4d1d776c80acc1278c1
Tobias Bosl <tboslffb@gmail.com>
2024-03-26 12:33:35 +0100

    feat: solve R-1.1.

---
+++ chapters/chapter1/reinforcement/R-1.1.py
@@ -0,0 +1,9 @@
+def is_multiple(n, m):
+    return n % m == 0
+
+
+print(is_multiple(5, 1))
+print(is_multiple(5, 2))
+print(is_multiple(9, 3))
+print(is_multiple(50, 10))
+print(is_multiple(22, 10))
