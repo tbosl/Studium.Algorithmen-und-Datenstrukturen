a93384828595b03d996b1cf1115f06cfadf8745c
Tobias Bosl <tboslffb@gmail.com>
2024-04-07 01:04:23 +0200

    solve R-3.23. [#3.23]

---
+++ problems/problem8/chapter3/reinforcement/R-3.23.py
@@ -0,0 +1,10 @@
+# Give a big-Oh characterization, in terms of n, of the running time of the
+# example1 function shown in Code Fragment 3.10
+# 1 def example1(S):
+# 2 ”””Return the sum of the elements in sequence S.”””
+# 3 n = len(S)
+# 4 total = 0
+# 5 for j in range(n): # loop from 0 to n-1
+# 6 total += S[j]
+# 7 return total
+# O(n)
