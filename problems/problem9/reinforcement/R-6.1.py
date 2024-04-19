65c3f4069a9d279d812e0771de47e6756fba966c
Tobias Bosl <tboslffb@gmail.com>
2024-04-19 15:23:31 +0200

    solve R-6.1 [#9.5]

---
+++ problems/problem9/reinforcement/R-6.1.py
@@ -0,0 +1,19 @@
+# What values are returned during the following series of stack operations, if
+# executed upon an initially empty stack?
+# push(5),
+# push(3),
+# pop(), -> 3
+# push(2),
+# push(8),
+# pop(), -> 8
+# pop(), -> 2
+# push(9),
+# push(1),
+# pop(), -> 1
+# push(7),
+# push(6),
+# pop(), -> 6
+# pop(), -> 7
+# push(4),
+# pop(), -> 4
+# pop(). -> 9
