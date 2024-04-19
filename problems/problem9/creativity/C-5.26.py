7dfdd72cc7fc196a8c210c23b671b4ee75504d7a
Tobias Bosl <tboslffb@gmail.com>
2024-04-19 15:17:56 +0200

    solve C-5.26 [#9.4]

---
+++ problems/problem9/creativity/C-5.26.py
@@ -0,0 +1,6 @@
+# Sorting the array from 1 to n
+# Check each element if the next element is the same
+
+# w/o sorting:
+# Create a set "spotted". Add all elements that are not contained yet.
+# If it is already contained then this is the first number.
