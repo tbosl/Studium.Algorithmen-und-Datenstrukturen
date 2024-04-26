89e2d08b198feb96bb61e87230032c460e115ecf
Tobias Bosl <tboslffb@gmail.com>
2024-04-26 15:30:40 +0200

    solve R-7.9

---
+++ problems/problem9/reinforcement/R-7.9.py
@@ -0,0 +1,3 @@
+# make the pointer of the head of the first list to the tail of the last list and vice versa
+# L.head -> M.tail, M.tail -> L.head
+# since it is not cirular, this is all that has to be done
