6b38af4e9937006c2b347c59cf5373664f5148ff
Tobias Bosl <tboslffb@gmail.com>
2024-06-11 23:50:59 +0200

    feat: implement requested changes; add graphic with binary attributes. [-]

--- problems/bonusproblem/util.py
+++ problems/bonusproblem/util.py
@@ -6,6 +6,4 @@ def unique_values(A, examples):
 
 def check_if_all_examples_have_same_classification(examples):
     """Returns True if all examples have the same classification, False otherwise."""
-    if not examples:
-        return True
     return len([e for e in examples if e.classification != examples[0].classification]) == 0
